71b63bbcadf48f846afdb8613422483f32d0c0fd
Leandro Farias <leandro.farias01@outlook.com>
2022-02-15 22:54:23 -0300

    feat(heroku): setup

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/boilerplate-angular'),
+      dir: require('path').join(__dirname, './coverage/act-front-end-challenge'),
       subdir: '.',
       reporters: [
         { type: 'html' },
